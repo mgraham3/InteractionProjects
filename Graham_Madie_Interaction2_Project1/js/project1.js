c45119031fe23821ab1e354f5e2ebb3e7fd997f8
mgraham3 <57780641+mgraham3@users.noreply.github.com>
2020-02-08 15:51:49 -0500

    Add files via upload

---
+++ Graham_Madie_Interaction2_Project1/js/project1.js
@@ -0,0 +1,235 @@
+//Week 4 Jquery: Animation & FadeOut Exp.1//
+//not fleeting fadeout//
+$(document).ready(function(){
+    $(".notfleeting").delay(1000).fadeOut("50000");
+});
+//ever animation to top//
+$(document).ready(function(){
+    $('.ever')
+        .delay(500)
+        .animate({top:'100px'}, 1000); 
+});
+//changinganimation to top//
+$(document).ready(function(){
+    $('.changing')
+        .delay(1000)
+        .animate({top:'100px'}, 1000); 
+});
+//ever fadeout//
+$(document).ready(function(){
+    $('.ever')
+        .delay(500)
+        .fadeOut("40000"); 
+});
+//changing fadeout//
+$(document).ready(function(){
+    $('.changing')
+        .delay(500)
+        .fadeOut("40000"); 
+});
+//everywhere animation to the top//
+$(document).ready(function(){
+    $('.everywhere')
+        .delay(3000)
+        .animate({top:'70px'}, 1000); 
+});
+
+//Week 4 Jquery: Animation & FadeTo Exp.2//
+//fish fade//
+$(document).ready(function(){
+    $('#fishy').fadeOut(6000, 0.0);
+    $('#fishy2').fadeOut(6000, 0.0);
+     $('#fishy3').fadeOut(6000, 0.0);
+     $('#fishy4').fadeOut(6000, 0.0);
+});
+
+//Week 4 Jquery: Animation & FadeIn Exp.3//
+//sunflower fadein//
+$(document).ready(function(){
+    $("#sunfloweranimate").hide().fadeIn(3000, 0.0);
+});
+
+//Week 4 Jquery: Animation & Delay Exp.4//
+$(document).ready(function(){
+    $(".lifeis")
+        .hide()
+        .delay(1000)
+        .fadeIn(1000)
+});
+
+//Week 4 Jquery: Animation & Scale Exp.5//
+$(document).ready(function(){
+    $(".heyu")
+        .delay(3000)
+        .animate({fontSize: '40pt'})
+});
+$(document).ready(function(){
+    $(".swim")
+        .delay(3000)
+        .animate({fontSize: '24pt'})
+});
+
+//Week 4 Jquery: Animation & String Exp.6//
+ $(".happyfish")
+        .hide()
+        .delay(1000)
+        .fadeIn(1000)
+
+//Week 4 Jquery: Animation & Subtle Exp.7//
+$(document).ready(function(){
+    $(".yousee")
+        .fadeTo(3000, 0.25)
+});
+
+//Week 4 Jquery: Animation & Chaotic Exp.9//
+$(document).ready(function loop(){
+    $('#fish1')
+        .delay(1000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish2')
+        .delay(2000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish3')
+        .delay(3000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish4')
+        .delay(4000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish5')
+        .delay(5000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish6')
+        .delay(6000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish7')
+        .delay(7000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish8')
+        .delay(8000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish9')
+        .delay(9000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish10')
+        .delay(10000)
+        .fadeOut(1000)
+});
+
+$(document).ready(function loop(){
+    $('#fish11')
+        .delay(11000)
+        .fadeOut(1000)
+});
+
+$(document).ready(function loop(){
+    $('#fish12')
+        .delay(12000)
+        .fadeOut(1000)
+});
+
+$(document).ready(function loop(){
+    $('#fish13')
+        .delay(13000)
+        .fadeOut(1000)
+});
+
+$(document).ready(function loop(){
+    $('#fish14')
+        .delay(14000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish15')
+        .delay(15000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish16')
+        .delay(16000)
+        .fadeOut(1000)
+});
+
+$(document).ready(function loop(){
+    $('#fish17')
+        .delay(17000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish18')
+        .delay(18000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish19')
+        .delay(19000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish20')
+        .delay(20000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish21')
+        .delay(21000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish22')
+        .delay(22000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish23')
+        .delay(23000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish24')
+        .delay(24000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish25')
+        .delay(25000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish26')
+        .delay(26000)
+        .fadeOut(1000)
+});
+$(document).ready(function loop(){
+    $('#fish27')
+        .delay(27000)
+        .fadeOut(1000)
+});
+$(document).ready(function(){
+    $(".plentyfish")
+        .delay(29000)
+        .animate({top: '40px', fontSize: '40pt'})
+});
+
+
+
+
+
+
